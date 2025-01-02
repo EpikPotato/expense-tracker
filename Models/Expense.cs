8074d05b1dfa630c99a775fae03004bfe6d541fc
EpikPotato <duccute3@gmail.com>
2025-01-02 22:31:02 +0700

    feat: adding RelayCommand

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -1,16 +1,33 @@
-public class Expense
+namespace expense.Models
 {
-    public string Name { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public double Amount { get; set; }
-    public Type Type { get; set; }
-
-    public Expense(string name, DateTime createdDate, double amount, Type type)
+    public class Expense
     {
-        Name = name;
-        CreatedDate = createdDate;
-        Amount = amount;
-        Type = type;
-    }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
 
+        public double Amount
+        {
+            get;
+            set
+            {
+                if (field < 0)
+                {
+                    throw new ArgumentException("Value must be positive");
+                }
+
+                field = value;
+            }
+        }
+
+        public Type Type { get; set; }
+
+        public Expense(string name, DateTime createdDate, double amount, Type type)
+        {
+            Name = name;
+            CreatedDate = createdDate;
+            Amount = amount;
+            Type = type;
+        }
+
+    }
 }
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using expense.Models;
+
+namespace expense.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ObservableCollection<Expense> Expenses { get; set; }
+        public RelayCommand AddExpense => new RelayCommand(execute => { AddItem();} , canExecute => { return true; });
+        
+        public MainViewModel()
+        {
+            Expenses = new ObservableCollection<Expense>();
+        }
+
+        private void AddItem()
+        {
+            Expenses.Add(new Expense("Food" , DateTime.Now , 500000 , Type.Expense));
+            Expenses.Add(new Expense("Salary" , DateTime.Now , 1100000 , Type.Income));
+
+        }
+    }
+}
